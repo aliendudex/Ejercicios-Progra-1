a6e2e39eb4d2bfa3b80b663e7ffb0ef1ebb8196c
Franco <franchialli@gmail.com>
2025-05-20 13:55:23 -0300

    Actualizo TP4

--- TP4.py
+++ TP4.py
@@ -13,17 +13,35 @@ def sumatoria(lista):
     suma=sum(lista)
     return suma
 
+def eliminarElemento(lista, num):
+    for element in lista:
+        if element == num:
+            lista.remove(element)
+
+def capicua(lista):
+    if lista == lista[::-1]:
+        return True
+    else:
+        return False
 
 
+def main():
+    '''
+    numList2=cargaLista2()
+    print(numList2)
+    '''
 
+    '''numList=cargaLista1()
+    print(numList)
+    sumaLista=sumatoria(numList)
+    print(sumaLista)
+    numAEliminar = int(input("Elegir numero a eliminar: "))
+    eliminarElemento(numList, numAEliminar)
+    print(numList)'''
 
-'''
-numList=cargaLista1()
-print(numList)
-numList2=cargaLista2()
-print(numList2)
-sumaLista=sumatoria(numList)
-'''
+    capList=[50,17,91,17,50]
+    esCap = capicua(capList)
+    print(esCap)
 
-
-
+if __name__=="__main__":
+    main()
